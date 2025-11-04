61568b1d619c314f72223f62b2d0bcecb9300da9
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-11-05 00:00:21 +0900

    db: MongoDB import, connect, db, collection 기초 사용법

--- app/page.js
+++ app/page.js
@@ -1,4 +1,11 @@
-export default function Home() {
+import { MongoClient } from "mongodb";
+
+export default async function Home() {
+
+  const client = await MongoClient.connect('mongodb+srv://admin:<password>@cluster-test.xlyfktc.mongodb.net/?appName=Cluster-test', { useNewUrlParser: true });
+  const db = client.db("forum");
+  const collection = db.collection("posts");
+
   return (
     <div>
         안녕
