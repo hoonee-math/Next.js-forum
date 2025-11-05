e459413e9e13571a2c724cc88bb0e272e430ba81
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-11-05 17:10:17 +0900

    feat: Link 태그 이용해서 상세페이지 연결해주기,  *** next/link의 Link는 default export라서 중괄호 {} 없이 import 해야 함.

--- app/list/page.js
+++ app/list/page.js
@@ -1,4 +1,5 @@
 import { connectDB } from "@/util/database.js"
+import Link from "next/link";
 
 export default async function List() {
   let db = (await connectDB).db('forum');
@@ -9,7 +10,7 @@ export default async function List() {
       {
         result.map((item,i) =>
           <div className="list-item" key={i}>
-            <h4>{item.title}</h4>
+            <Link href={'/detail/' + item._id}><h4>{item.title}</h4></Link>
             <p>{item.content}</p>
           </div>
         )
