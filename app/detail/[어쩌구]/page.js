ef551b7603b31522ed651a7fb2dce44ecd145b44
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-11-05 16:33:42 +0900

    db: mongoDB의 _id 를 이용해서 값 찾아오기, ObjectId import 해서 사용하기

--- app/detail/[어쩌구]/page.js
+++ app/detail/[어쩌구]/page.js
@@ -1,10 +1,11 @@
 // detail/[어쩌구]/page.js
 import { connectDB } from "@/util/database.js"
+import { ObjectId } from "mongodb";
 
 export default async function Detail() {
   let db = (await connectDB).db('forum');
-  let result = await db.collection('post').findOne({ title : '안녕' });
-  console.log(result);
+  let result = await db.collection('post').findOne({ _id : new ObjectId("690acf65d4dcebc7d2309e12") });
+  console.log("result: ", result);
 
   return (
     <div>
