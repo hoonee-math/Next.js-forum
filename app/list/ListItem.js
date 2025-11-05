ec0ef88b6c57faba1bc7b6c3e72559b014c32dcf
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-11-05 23:24:16 +0900

    design: 삭제 후 html 요소 제거하는 애니메이션 효과 주기
    1. 애니메이션 동작 전, 동작 후의 CSS 스타일을 생각해보고
    
    2. 애니메이션 동작 전 스타일 넣어주고
    
    3. transition 스타일도 넣어주고
    
    4. 원하는 시점에 애니메이션 동작 후 스타일을 넣으면 끝임
    - onClick 매개변수에 e를 추가하면, e.target은 유저가 현재 클릭한 html 요소를 알려줌
    - .parentElement로 부모 요소를 찾아줌.
    - style.opacity 로 스타일을 바꿔줄 수 있음

--- app/list/ListItem.js
+++ app/list/ListItem.js
@@ -4,16 +4,7 @@ import Link from "next/link";
 import DetailLink from "./DetailLink";
 import { useEffect } from "react";
 
-export default function ListItem(props) { // props를 편하게 사용하기 위한 destructuring 문법도 가능: ({result}) -> { ... props.result => result
-
-  useEffect(() => {
-    // 클라이언트 사이드에서 실행되는 코드
-    // api 요청을 보내서 데이터를 가져올 수 있습니다.
-    // client component 에서는 검색 노출에 유리한 SEO 최적화가 어렵습니다.
-
-    // 따라서 검색엔진 노출이 중요한 경우에는 서버 컴포넌트에서 DB 데이터를 가져온 다음에 props로 넘겨주는 방식을 권장합니다.
-  }, []);
-
+export default function ListItem(props) {
   return (
     <div>
       {
@@ -22,28 +13,17 @@ export default function ListItem(props) { // props를 편하게 사용하기 위
             <Link href={'/detail/' + item._id}><h4>{item.title}</h4></Link>
             <Link href={'/edit/' + item._id}> ✏️ </Link>
             {/* form 태그 말고도 서버에 Http 요청 보내는 방법: Ajax */}
-            <span onClick={()=>{
+            <span onClick={(e)=>{
               fetch('/api/post/delete',{ method: 'DELETE', body: item._id })
                 .then(res => res.json())
                 .then(data => {
                   console.log(data.message);
                   alert(data.message);
+                  e.target.parentElement.style.opacity = 0;
+                  setTimeout(()=>{
+                    e.target.parentElement.style.display = 'none';
+                  },1000)
                 })
-              // 문자나 숫자는 그냥 body에 넣어도 상관 없지만, Array 또는 Object 객체는 JSON.stringify() 로 묶어줘야함.
-              // fetch('/URL')
-              //   .then((r)=>{
-              //     if(r.status == 200) {
-              //       return r.json()
-              //     } else {
-              //       //서버가 에러코드전송시 실행할코드
-              //     }
-              //   })
-              //   .then((result)=>{
-              //     //성공시 실행할코드
-              //   }).catch((error)=>{
-              //     //인터넷문제 등으로 실패시 실행할코드
-              //     console.log(error)
-              //   })
             }}>🗑️</span>
             <p>{item.content}</p>
             <DetailLink />
