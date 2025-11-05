d0e5170e236b3ba0386160d18f1e086e55d5adbf
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-11-05 16:56:23 +0900

    feat: props 로 각 id 값을 이용해서 받아오기, ObjectId 정확히 사용하기 (String으로 변환 후 이용)

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -1,16 +1,21 @@
-// detail/[어쩌구]/page.js
+// detail/[id]/page.js
 import { connectDB } from "@/util/database.js"
+import { ObjectId } from "mongodb"
 
-export default async function Detail() {
+export default async function Detail(props) {
+  console.log("==== props: ",props); // { params: { id: '2' }, searchParams: {} }
+  const id = String(props.params.id);
+
   let db = (await connectDB).db('forum');
-  let result = await db.collection('post').findOne({ _id : props.params.id });
+  let result = await db.collection('post').findOne({ _id : new ObjectId(id) });
+
   console.log("result: ", result);
 
   return (
     <div>
       <h4>상세페이지</h4>
-      <h4>글제목</h4>
-      <p>글내용</p>
+      <h5>{result.title}</h5>
+      <p>{result.content}</p>
     </div>
   );
 }
