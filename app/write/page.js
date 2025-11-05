aea384f146c5c5cd2bb246edde97119423d7c1ef
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-11-05 20:59:13 +0900

    feat: 저장 후 리다이렉트 성공

--- app/write/page.js
+++ app/write/page.js
@@ -1,10 +1,10 @@
 export default function Write() {
   return (
-    <div>
+    <div className="p-20">
       <h1>글 작성 페이지</h1>
       <form action="/api/post" method="POST"> {/* GET, POST 만 사용가능 */}
         <input name="title" placeholder="제목" /><br />
-        <textarea name="content" placeholder="내용"></textarea><br />
+        <input name="content" placeholder="내용"></input><br />
         <button type="submit">완료</button>
       </form>
     </div>
--- pages/api/post.js
+++ pages/api/post.js
@@ -1,12 +1,15 @@
 import { connectDB } from "@/util/database";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'POST') {
+    console.log(req.body);
     const data = req.body;
-    connectDB.then((db) => {
+    connectDB.then(async (db) => {
       const dbo = db.db("forum");
-      dbo.collection("post").insertOne(data);
-      res.status(200).json('POST 요청 성공');
+      const savedData = await dbo.collection("post").insertOne(data);
+      console.log("데이터 저장 완료", savedData);
+      // return res.status(200).json('POST 요청 성공');
+      return res.status(200).redirect('/detail/'+savedData.insertedId);
     });
 
   }
