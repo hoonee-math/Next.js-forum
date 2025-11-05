d711b94c49e3b88c789941a5d0813a758bf1b906
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-11-05 16:46:53 +0900

    db: ObjectId 방식 개선됨 - deprecated(사용 중단 예정)
    https://chatgpt.com/share/690b0048-04c4-8008-bd5c-6649aa38f85b

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -1,10 +1,9 @@
 // detail/[어쩌구]/page.js
 import { connectDB } from "@/util/database.js"
-import { ObjectId } from "mongodb";
 
 export default async function Detail() {
   let db = (await connectDB).db('forum');
-  let result = await db.collection('post').findOne({ _id : new ObjectId("690acf65d4dcebc7d2309e12") });
+  let result = await db.collection('post').findOne({ _id : props.params.id });
   console.log("result: ", result);
 
   return (
