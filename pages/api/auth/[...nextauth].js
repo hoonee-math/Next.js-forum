29b633fea57c16c999383b8afa2c417ae1c5ded1
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-11-07 17:37:52 +0900

    feat(social): NextAuth.js install, pages/api/'auth/[...nextauth].js 구현
    ## 강의 4.21.1 버전 설치 실패
    ```
    npm install next-auth@4.21.1
    ```
    - 원인: 4.21.1은 13 버전에서 사용하던 것
    - 해결: latest 로 설치
    - 참고: https://next-auth.js.org/

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth from 'next-auth'
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from 'next-auth/providers/google'
+
+export default NextAuth({
+  providers: [
+    // https://next-auth.js.org/
+    // OAuth authentication providers...
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRETE
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+  ],
+  secret : process.env.JWT_SECRET
+})
