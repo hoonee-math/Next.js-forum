568c4d8ec92288f52bd025534f1d4c52e2c10feb
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-11-10 19:26:30 +0900

    feat(comment): 댓글 Get 요청 추가, useState는 한 번에 하나의 state만 생성함, 두 개의 상태를 관리하려면 따로 작성해줘야 함.

--- app/detail/[id]/Comment.js
+++ app/detail/[id]/Comment.js
@@ -1,13 +1,40 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Comment(props) {
   const postId = props.postId;
   let [comment, setComment] = useState('')
+  let [comments, setComments] = useState([])
+
+  // useEffect
+  // 특징 1: html 로드/재렌더링 될 때마다 실행
+  // 특징 2: 두번째 인자(배열)가 비어있으면 최초 1회만 실행
+  // 특징 3: return의 html 보여준 후에 늦게 실행됨
+  // why? (ajax 요청완료 후에 보여줘야 하는거 아닌가?) => 사용자 경험 향상 위해서
+  // 동작 순서: 1. html 렌더링 -> 2. useEffect 실행 -> 3. ajax 완료 시점에 state 변경 -> 4. 재렌더링
+  useEffect(()=>{
+    fetch(`/api/post/${postId}/comment`, { method:'GET' })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log("💬 댓글 데이터: ", data)
+      setComments(data)
+    })
+  },[])
+
   return (
     <div>
       <h4>댓글영역</h4>
+      {
+        comments.length > 0
+        ? comments.map((comment, i) => (
+            <div key={i}>
+              <p>{comment.content}</p>
+              <p>작성자: {comment.author}</p>
+            </div>
+          ))
+        : <p>댓글이 없습니다.</p>
+      }
       <div>
         <input onChange={(e)=>{ setComment(e.target.value) }} />
         <button onClick={()=>{
@@ -16,5 +43,5 @@ export default function Comment(props) {
       </div>
     </div>
   );
-}
+   }
 
--- pages/api/post/[id]/comment.js
+++ pages/api/post/[id]/comment.js
@@ -8,18 +8,29 @@ export default async function handler(req, res) {
   if(req.method === 'POST'){
     console.log("📢 /api/post/{id}/comment API POST 요청 들어옴 ✨");
     const postId = req.query.id;
-    console.log("📢 req.body: ", req.body);
-    console.log("📢 req.query.id: ", postId);
 
     try{
       const session = await getServerSession(req, res, authOptions);
       if (!session) return res.status(401).json({ message: '로그인이 필요합니다.' });
-      console.log("📢 /api/post/{id}/comment session: ", session.user.email );
 
       const db = (await connectDB).db('forum');
       let result = await db.collection('comment').insertOne( { content: req.body, author:session.user.email ,parent: new ObjectId(postId) } );
-      console.log("✔️ 댓글 등록 결과: ", result );
+
       res.status(200).json({ message: "댓글 등록 완료" })
+    } catch {
+      res.status(500).json({ message: "서버 내부 오류" })
+    }
+  }
+
+  if(req.method === 'GET'){
+    console.log("📢 /api/post/{id}/comment API GET 요청 들어옴 ✨");
+    const postId = req.query.id;
+
+    try{
+      const db = (await connectDB).db('forum');
+      let comments = await db.collection('comment').find( { parent: new ObjectId(postId) } ).toArray();
+
+      res.status(200).json(comments)
     } catch {
       res.status(500).json({ message: "서버 내부 오류" })
     }
