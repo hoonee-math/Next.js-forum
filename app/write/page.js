f53ade1ebee84941c6f282a849518a81dcde370d
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-11-12 12:10:58 +0900

    feat(image): Presigned URL 기반 image 업로드하기 1단계 (1) 서버에 Presigned URL 만들어달라고 요청하기

--- app/write/page.js
+++ app/write/page.js
@@ -1,3 +1,5 @@
+'use client'
+
 import { authCheck } from "@/util/authCheck"
 
 export default async function Write() {
@@ -10,6 +12,16 @@ export default async function Write() {
       <form action="/api/post" method="POST"> {/* GET, POST 만 사용가능 */}
         <input name="title" placeholder="제목" /><br />
         <input name="content" placeholder="내용"></input><br />
+        {/* type에 file 설정 가능, 허용 가능한 파일 유형 선택 가능 */}
+        <input type="file" accept="image/*"
+          onChange={async(e)=>{
+            let file = e.target.files[0]; // e.target.files: 선택된 파일 목록
+            let filename = encodeURIComponent(file.name) //한글 파일명이 깨질 수 있기 때문에 encodeURIComponent 사용
+            let res = await fetch('/api/post/image'+'?file='+filename)
+            res = await res.json()
+          }}
+        />
+        <img src=""/>
         <button type="submit">완료</button>
       </form>
     </div>
