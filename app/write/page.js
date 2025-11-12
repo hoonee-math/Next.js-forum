7dd4fd90d68d68d1dfc74198495406d31962f512
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-11-12 12:42:57 +0900

    feat(image): Presigned URL 기반 image 업로드하기 3단계 - 등록한 이미지를 기반으로 이미지 불러와서 출력해주기 (추가 과제: 이제 form 대신 ajax)
    - client component라서 글발행시 ajax요청으로 폼전송하는게 어떨까요
    
    - 글의 상세페이지에선 글과 함께 업로드한 사진도 보여야합니다.

--- app/write/page.js
+++ app/write/page.js
@@ -15,13 +15,34 @@ export default function Write() {
         {/* type에 file 설정 가능, 허용 가능한 파일 유형 선택 가능 */}
         <input type="file" accept="image/*"
           onChange={async(e)=>{
-            let file = e.target.files[0]; // e.target.files: 선택된 파일 목록
-            let filename = encodeURIComponent(file.name) //한글 파일명이 깨질 수 있기 때문에 encodeURIComponent 사용
-            let res = await fetch('/api/post/image'+'?file='+filename)
-            res = await res.json()
+            try{
+              let file = e.target.files[0]; // e.target.files: 선택된 파일 목록
+              let filename = encodeURIComponent(file.name) //한글 파일명이 깨질 수 있기 때문에 encodeURIComponent 사용
+              let res = await fetch('/api/post/image'+'?file='+filename)
+              res = await res.json()
+
+              //S3 업로드
+              const formData = new FormData()
+              Object.entries({ ...res.fields, file }).forEach(([key, value]) => {
+                formData.append(key, value)
+              })
+              let 업로드결과 = await fetch(res.url, {
+                method: 'POST',
+                body: formData,
+              })
+              console.log(업로드결과)
+
+              if (업로드결과.ok) {
+                setSrc(업로드결과.url + '/' + filename)
+              } else {
+                console.log('실패')
+              }
+            } catch(error) {
+              console.log('실패:', error)
+            }
           }}
         />
-        <img src=""/>
+        <img src={src}/>
         <button type="submit">완료</button>
       </form>
     </div>
